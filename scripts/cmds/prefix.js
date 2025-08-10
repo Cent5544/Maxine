f738b80fb63bfa6f0e75c2bb73074e1519eca383
Vincent <142700197+Cent5544@users.noreply.github.com>
2025-08-10 16:37:29 +0800

    prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "Yo senpai 🐔\n\nMy prefix is: %1\nYor group chat prefix is: %2"
+			myPrefix: "Hey kupal you dont know how to use my commands? \n\nMy prefix is: %1\nYor group chat prefix is: %2 \nexample; %1help"
 		}
 	},
 
